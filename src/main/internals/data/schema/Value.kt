a6f14d7f1e31d72327b9291d590b44f4dfc6cc2c
Lyblinskii Alexander <lyblinskii.shx@gmail.com>
2017-01-15 10:26:04 +0300

    dev: refactor: make Value factory method creation

--- src/main/internals/data/schema/Value.kt
+++ src/main/internals/data/schema/Value.kt
@@ -1,24 +1,38 @@
 package main.internals.data.schema
 
 abstract class Value<out T>(val value: T?) {
+    companion object {
+        fun of(value: Int?) = object : Value<Int>(value) {
+            override fun getType(): Type {
+                return Type.int32
+            }
+        }
+
+//        TODO: figure out bug with resolve ambiguity for case Value.of(1)
+//        fun ofLong(value: Long?) = object : Value<Long>(value) {
+//            override fun getType(): Type {
+//                return Type.int64
+//            }
+//        }
+
+        fun of(value: String?) = object : Value<String>(value) {
+            override fun getType(): Type {
+                return Type.string
+            }
+        }
+
+        fun of(value: Boolean?) = object : Value<Boolean>(value) {
+            override fun getType(): Type {
+                return Type.boolean
+            }
+        }
+
+        fun of(value: Double?) = object : Value<Double>(value) {
+            override fun getType(): Type {
+                return Type.double
+            }
+        }
+    }
+
     abstract fun getType(): Type
 }
-
-class StringValue(value: String?) : Value<String>(value) {
-    override fun getType(): Type {
-        return Type.string
-    }
-}
-
-class Int32Value(value: Int?) : Value<Int>(value) {
-    override fun getType(): Type {
-        return Type.int32
-    }
-}
-
-class Int64Value(value: Long?) : Value<Long>(value) {
-    override fun getType(): Type {
-        return Type.int64
-    }
-}
-
